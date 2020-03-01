486c98e3c0f1a0d028d6ad71ddd8dfe100b32fe7
Andrey Ioch <dird15@mail.ru>
2020-03-01 15:56:44 +0300

    Added user service

---
+++ Corvette.Chat.Services/IUserService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Corvette.Chat.Data;
+using Corvette.Chat.Services.DTO;
+using Corvette.Chat.Services.Exceptions;
+
+namespace Corvette.Chat.Services
+{
+    /// <summary>
+    /// Service that contains all of the user business logic.
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Creates a new chat user.
+        /// </summary>
+        /// <param name="name">Unique user name</param>
+        /// <exception cref="ArgumentOutOfRangeException">When the name is null or white space</exception>
+        /// <exception cref="ChatServiceException">When a user with that name is already exist</exception>
+        Task<UserDto> CreateAsync(string name);
+
+        /// <summary>
+        /// Updates the user's name.
+        /// </summary>
+        /// <param name="userId">User id</param>
+        /// <param name="name">User's new name</param>
+        /// <exception cref="ArgumentOutOfRangeException">When user id is default or user name is null or white space</exception>
+        /// <exception cref="ChatServiceException">When a user with that name is already exist</exception>
+        /// <exception cref="EntityNotFoundException">When a user was not found by id in the database</exception>
+        Task UpdateAsync(Guid userId, string name);
+        
+        /// <summary>
+        /// Returns a user by id.
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <exception cref="EntityNotFoundException">When a user was not found by id in the database</exception>
+        /// <exception cref="ArgumentOutOfRangeException">When user id is default</exception>
+        Task<UserDto> GetAsync(Guid id);
+
+        /// <summary>
+        /// Removes users from the database.
+        /// </summary>
+        /// <param name="userIds">List of user ids which will remove</param>
+        /// <exception cref="ArgumentOutOfRangeException">When a users id list is empty</exception>
+        /// <exception cref="EntityNotFoundException">When a user was not found by id in the database</exception>
+        Task RemoveAsync(IReadOnlyList<Guid> userIds);
+
+        /// <summary>
+        /// Returns true when a user with the current name is already exist in the database.
+        /// </summary>
+        /// <param name="username">User name</param>
+        /// <exception cref="ArgumentOutOfRangeException">When the username is null or white space</exception>
+        Task<bool> IsUserNameUsedAsync(string username);
+    }
+}
---
+++ Corvette.Chat.Services/Impl/UserService.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Corvette.Chat.Data;
+using Corvette.Chat.Data.Entities;
+using Corvette.Chat.Services.DTO;
+using Corvette.Chat.Services.Exceptions;
+using Corvette.Chat.Services.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Corvette.Chat.Services.Impl
+{
+    /// <inheritdoc/>
+    public sealed class UserService : IUserService
+    {
+        private readonly ILogger<UserService> _logger;
+
+        private readonly ChatDataContextFactory _contextFactory;
+
+        public UserService(
+            ILogger<UserService> logger, 
+            ChatDataContextFactory contextFactory)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<UserDto> CreateAsync(string name)
+        {
+            await using var context = _contextFactory.CreateContext();
+
+            if (string.IsNullOrWhiteSpace(name)) 
+                throw new ArgumentOutOfRangeException(nameof(name), "Can't be null or empty.");
+            
+            if (await IsUserNameUsedAsync(context, name)) 
+                throw new ChatServiceException($"A user name: {name} is already used.");
+
+            
+            var user = new UserEntity
+            {
+                Name = name,
+            };
+
+            context.Add(user);
+            await context.SaveChangesAsync();
+            
+            _logger.LogInformation($"User with id: {user.Id}, name: {user.Name} was successfully created.");
+
+            return new UserDto(user);
+        }
+
+        /// <inheritdoc/>
+        public async Task UpdateAsync(Guid userId, string name)
+        {
+            await using var context = _contextFactory.CreateContext();
+
+            if (userId == default) 
+                throw new ArgumentOutOfRangeException(nameof(userId), "Can't be default.");
+            if (string.IsNullOrWhiteSpace(name)) 
+                throw new ArgumentOutOfRangeException(nameof(name), "Can't be null or empty.");
+            
+            if (await IsUserNameUsedAsync(context, name, userId))
+                throw new ChatServiceException($"A user name: {name} is already used.");
+
+            
+            var user = await context.Users.GetByIdAsync(userId);
+            user.Name = name;
+            
+            _logger.LogInformation($"Username: {user.Name} was successfully updated for user with id: {user.Id}.");
+
+            await context.SaveChangesAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<UserDto> GetAsync(Guid id)
+        {
+            if (id == default) throw new ArgumentOutOfRangeException(nameof(id), "Can't be default.");
+            
+            
+            await using var context = _contextFactory.CreateContext();
+            var user = await context.Users.GetByIdAsync(id, true);
+            
+            return new UserDto(user);
+        }
+
+        /// <inheritdoc/>
+        public async Task RemoveAsync(IReadOnlyList<Guid> userIds)
+        {
+            if (userIds.Count <= 0) throw new ArgumentOutOfRangeException(nameof(userIds), "Can't be empty.");
+            
+            
+            await using var context = _contextFactory.CreateContext();
+            foreach (var userId in userIds)
+            {
+                var user = await context.Users.GetByIdAsync(userId);
+                context.Users.Remove(user);
+            }
+
+            await context.SaveChangesAsync();
+            
+            _logger.LogInformation($"Users with id[{string.Join(" ,", userIds)}] was successfully removed");
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> IsUserNameUsedAsync(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentOutOfRangeException(nameof(username), "Can't be null or empty.");
+
+            await using var context = _contextFactory.CreateContext();
+            return await IsUserNameUsedAsync(context, username);
+        }
+
+        private async Task<bool> IsUserNameUsedAsync(ChatDataContext context, string username, Guid? userId = null)
+        {
+            return await context.Users
+                .Where(x => x.Name.Equals(username.Trim(), StringComparison.OrdinalIgnoreCase))
+                .Where(x => userId == null || x.Id != userId)
+                .AnyAsync();
+        }
+    }
+}
