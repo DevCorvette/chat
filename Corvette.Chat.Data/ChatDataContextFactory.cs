b36b1bc8b9cafe401dbce7f47c548d31fbcd2641
Andrey Ioch <dird15@mail.ru>
2020-03-01 15:48:01 +0300

    Added chat data context factory

---
+++ Corvette.Chat.Data/ChatDataContextFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Corvette.Chat.Data
+{
+    /// <summary>
+    /// Factory which creates <see cref="ChatDataContext"/>.
+    /// </summary>
+    public sealed class ChatDataContextFactory
+    {
+        private readonly string _connectionString;
+        
+        public ChatDataContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        /// <summary>
+        /// Returns a new <see cref="ChatDataContext"/>.
+        /// </summary>
+        public ChatDataContext CreateContext()
+        {
+            return new ChatDataContext(_connectionString);
+        }
+    }
+}
