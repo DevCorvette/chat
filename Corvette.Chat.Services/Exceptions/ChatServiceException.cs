06cfefb007d76365a97f6dca08391e3dc48ccb4b
Andrey Ioch <dird15@mail.ru>
2020-03-01 15:54:23 +0300

    Added business logic exceptions

---
+++ Corvette.Chat.Services/Exceptions/ChatServiceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Corvette.Chat.Services.Exceptions
+{
+    /// <summary>
+    /// Base exception for each exception of the chat business logic layer.
+    /// </summary>
+    public class ChatServiceException : Exception
+    {
+        public ChatServiceException(string message) : base(message)
+        {
+        }
+    }
+}
